b8f8e9a5606cebfc1cd1afd73ed6bee99f9b1817
atlaryea <manyeadorkor5@gmail.com>
2025-07-31 16:14:33 +0000

    finished the fetch api to send data to the frontend

--- index.js
+++ index.js
@@ -6,4 +6,12 @@ async function checkWeather() {
     var data = await response.json();
 
     console.log(data)
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+
 }
+
+checkWeather()
