cb4510dc0995d0dabd6d8e6f1119ee3f1f267087
atlaryea <manyeadorkor5@gmail.com>
2025-07-31 16:51:41 +0000

    added the code for the search function

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const apiKey = "7725f2789c08565b365866e1ce2e288f"
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=accra"
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q="
 
-async function checkWeather() {
-    const response  =  await  fetch(apiUrl + `&appid=${apiKey}`)
+const searchBox = document.querySelector(".search input")
+const searchBtn = document.querySelector(".search button")
+
+async function checkWeather(city) {
+    const response  =  await  fetch(apiUrl + city + `&appid=${apiKey}`)
     var data = await response.json();
 
     console.log(data)
@@ -11,7 +14,10 @@ async function checkWeather() {
     document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
     document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
-
 }
 
-checkWeather()
+searchBtn.addEventListener("click", ()=>{
+    checkWeather(searchBox.value)
+})
+
+checkWeather(city)
