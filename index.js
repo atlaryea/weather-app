c48ea5d32995894ef5917effa32e218ec4f72b9f
atlaryea <manyeadorkor5@gmail.com>
2025-07-31 15:53:20 +0000

    wrote the fetch api

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-const apiKey = ""
-const apiUrl = ""
+const apiKey = "7725f2789c08565b365866e1ce2e288f"
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=accra"
+
+async function checkWeather() {
+    const response  =  await  fetch(apiUrl + `&appid=${apiKey}`)
+    var data = await response.json();
+
+    console.log(data)
+}
